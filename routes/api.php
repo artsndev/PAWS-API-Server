7d2ff1671628943b091c97d71147cf0222b2bed8
Al-Fhaigar J. Usman <rubickking04@gmail.com>
2024-12-11 18:53:08 +0800

    coded by artisandev

--- routes/api.php
+++ routes/api.php
@@ -122,6 +122,7 @@ Route::middleware(['auth:user-api'])->group(function () {
         Route::get('/user/pet/{id}', 'show');
         Route::post('/user/pet/{id}', 'update');
         Route::delete('/user/pet/{id}', 'destroy');
+        Route::post('/user/upload', 'upload_pet_avatar');
     });
     // Appointment Data Route
     Route::controller(UserAppointmentController::class)->group(function () {
